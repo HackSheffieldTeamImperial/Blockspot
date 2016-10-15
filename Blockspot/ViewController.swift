92705a99787f938b7f4b964f33c95e6059d38781
Alessandro Bonardi <alebonardi@hotmail.it>
2016-10-15 14:54:36 +0100

    Initial Commit

---
+++ Blockspot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blockspot
+//
+//  Created by Alessandro Bonardi on 15/10/2016.
+//  Copyright © 2016 Alessandro Bonardi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
