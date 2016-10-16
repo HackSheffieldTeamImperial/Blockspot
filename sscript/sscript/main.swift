63a36185caa8e57e0de12b113caa8eb270b9a0c8
Rakhilya Mekhtieva Lee <admin_@dyn204089.shef.ac.uk>
2016-10-16 08:54:37 +0100

    File I/O and bash from Swift command line tool

---
+++ sscript/sscript/main.swift
@@ -0,0 +1,42 @@
+#!/usr/bin/swift
+
+import Foundation
+
+import Foundation
+//run bash in swift
+func shell(args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
+shell(args: "ls")
+shell(args: "sh", "script")
+
+
+
+//file I/O
+let file = "blacklist.txt" //this is the file. we will write to and read from it
+
+let text = "some text" //just a text
+
+if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+    
+    let path = dir.appendingPathComponent(file)
+    
+    //writing
+    do {
+        try text.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+    }
+    catch {/* error handling here */}
+    
+    //reading
+    do {
+        let contents = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        print(contents)
+    }
+    catch {/* error handling here */}
+}
